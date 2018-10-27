00ccc83ac88aeb367d0d56c9e9b0e3cd59601aa0
zhud2xx <zhud2xx@cse121596.uta.edu>
2018-10-27 14:43:52 -0500

    aa
    Change-Id: I7b98ffeeaf76e870dff90a7d26f215dc6dce7bd8

---
+++ src/edu/uta/cse/conference/ISBI2019/J_CombineSurface.java
@@ -0,0 +1,51 @@
+package edu.uta.cse.conference.ISBI2019;
+
+import edu.uga.liulab.djVtkBase.djVtkSurData;
+
+public class J_CombineSurface {
+	private djVtkSurData surData1;
+	private djVtkSurData surData2;
+	
+	private void loadData(String surFile1, String surFile2)
+	{
+		surData1 = new djVtkSurData(surFile1);
+		surData2 = new djVtkSurData(surFile2);
+	}
+	
+	private void combineSurfaces(String ourPutSur)
+	{
+		int startPtIndex = surData1.nPointNum;
+		for(int p=0;p<surData2.nPointNum;p++)
+		{
+			surData2.getPoint(p).pointId = startPtIndex+p;
+			surData1.points.add( surData2.getPoint(p) );
+		}
+		
+		int startCellIndex = surData1.nCellNum;
+		for(int c=0;c<surData2.nCellNum;c++)
+		{
+			surData2.getcell(c).cellId += startCellIndex+c;
+			surData1.cells.add( surData2.getcell(c) );
+		}
+		
+		surData1.nPointNum = surData1.nPointNum + surData2.nPointNum;
+		surData1.nCellNum = surData1.nCellNum + surData2.nCellNum;
+		surData1.cellsOutput.addAll(surData1.getAllCells());
+		surData1.writeToVtkFile(ourPutSur);
+	}
+
+	public static void main(String[] args) {
+		if(args.length!=3)
+		{
+			System.out.println("Need para: sur1 sur2 outSuf");
+			System.exit(0);
+		}
+		J_CombineSurface mainHandler = new J_CombineSurface();
+		String surFileName1 = args[0].trim();
+		String surFileName2 = args[1].trim();
+		String outputFileName = args[2].trim();
+		mainHandler.loadData(surFileName1, surFileName2);
+		mainHandler.combineSurfaces(outputFileName);
+	}
+
+}
