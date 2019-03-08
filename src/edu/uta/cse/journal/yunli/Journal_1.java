b9287e85f79eaafd216f134bd418085bed210971
dajiangzhu <dajiangzhu@cse121957.frontierlocal.net>
2019-03-08 08:23:21 -0600

    aaa

---
+++ src/edu/uta/cse/journal/yunli/Journal_1.java
@@ -0,0 +1,15 @@
+package edu.uta.cse.journal.yunli;
+
+public class Journal_1 {
+	
+	public void generateDicccolConn (String connFileName)
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
